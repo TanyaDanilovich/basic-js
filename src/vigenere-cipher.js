504b8c7a35888254aa02869f661cbf1bf0c11db4
TanyaDanilovich <tan.danilovich@gmail.com>
2022-05-01 23:45:45 +0300

    feat: add some solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	constructor(isNotReverse = true) {
+		this.type = isNotReverse;
+		this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	}
+
+	encrypt(...arrOfarguments) {
+		if (arrOfarguments.length !== 2 || typeof arrOfarguments[0] !== 'string' || typeof arrOfarguments[1] !== 'string') throw new Error('Incorrect arguments!');
+		let repeatCount = Math.ceil(arrOfarguments[0].length / arrOfarguments[1].length)
+		let upperText = arguments[0].toUpperCase()
+		let key = arrOfarguments[1].repeat(repeatCount).toUpperCase()
+		let res = []
+		let j = 0
+
+
+		for (let i = 0; i < arrOfarguments[0].length; i++) {
+			if (this.alphabet.includes(upperText[i])) {
+				let newAlphabet = this.alphabet.slice(this.alphabet.indexOf(key[j])) + this.alphabet;
+				res.push(newAlphabet[this.alphabet.indexOf(upperText[i])])
+				j++;
+			} else (
+				res.push(upperText[i])
+			)
+		}
+		return this.type ? res.join('') : res.reverse().join('');
+	}
+	decrypt(...arrOfarguments) {
+		if (arrOfarguments.length !== 2 || typeof arrOfarguments[0] !== 'string' || typeof arrOfarguments[1] !== 'string') throw new Error('Incorrect arguments!');
+		let repeatCount = Math.ceil(arrOfarguments[0].length / arrOfarguments[1].length)
+		let upperText = arrOfarguments[0].toUpperCase()
+		let key = arrOfarguments[1].repeat(repeatCount).toUpperCase()
+		let res = []
+		let j = 0
+		for (let i = 0; i < arrOfarguments[0].length; i++) {
+			if (this.alphabet.includes(upperText[i])) {
+				let newAlphabet = this.alphabet.slice(this.alphabet.indexOf(key[j])) + this.alphabet;
+				res.push(this.alphabet[newAlphabet.indexOf(upperText[i])])
+				j++;
+			} else {
+				//	console.log(i);
+				res.push(upperText[i])
+			}
+		}
+		return this.type ? res.join('') : res.reverse().join('');
+	}
 }
 
 module.exports = {
-  VigenereCipheringMachine
+	VigenereCipheringMachine
 };
